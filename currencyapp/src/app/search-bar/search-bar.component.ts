14efbc173f6029ff07a7368e542faebd63304165
sweepez <derlo@live.se>
2019-05-24 09:39:26 +0200

    datalist added

--- currencyapp/src/app/search-bar/search-bar.component.ts
+++ currencyapp/src/app/search-bar/search-bar.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-search-bar',
+  selector: 'app-searchbar',
   templateUrl: './search-bar.component.html',
   styleUrls: ['./search-bar.component.css']
 })
@@ -9,7 +9,57 @@ export class SearchBarComponent implements OnInit {
 
   constructor() { }
 
+  public currencies = [
+    "AUD",
+    "BGN",
+    "BRL",
+    "CAD",
+    "CHF",
+    "CNY",
+    "CZK",
+    "DKK",
+    "EUR",
+    "GBP",
+    "HKD",
+    "HRK",
+    "HUF",
+    "IDR",
+    "ILS",
+    "INR",
+    "ISK",
+    "JPY",
+    "KRW",
+    "MXN",
+    "MYR",
+    "NOK",
+    "NZD",
+    "PHP",
+    "PLN",
+    "RON",
+    "RUB",
+    "SEK",
+    "SGD",
+    "THB",
+    "TRY",
+    "USD",
+    "ZAR"
+
+  ];
+
   ngOnInit() {
+    this.printCurrencies();
+  }
+
+  printCurrencies = function () {
+    let datalistCurrencies = document.getElementById("currencies");
+    let currenciesOptions = '';
+    for (var i = 0; i < this.currencies.length; i++) {
+      console.log(this.currencies[i]);
+      currenciesOptions += '<option value="' + this.currencies[i] + '"></option>';
+      console.log(datalistCurrencies);
+    }
+
+    datalistCurrencies.innerHTML = currenciesOptions;
   }
 
 }
