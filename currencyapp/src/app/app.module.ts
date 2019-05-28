6ecf05f45b2d8443e93b2c84c6fc84cd22560f31
AntonNorberg <an.norberg98@gmail.com>
2019-05-28 12:03:40 +0200

    Jesper fucka upp nåt, jag fixar som vanligt

--- currencyapp/src/app/app.module.ts
+++ currencyapp/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { UiSwitchModule } from 'ngx-toggle-switch';
 
 import { AppComponent } from './app.component';
 import { RatesComponent } from './rates/rates.component';
@@ -14,6 +15,7 @@ import { ConverterComponent } from './converter/converter.component';
   ],
   imports: [
     BrowserModule,
+    UiSwitchModule,
     HttpClientModule
   ],
   providers: [],
